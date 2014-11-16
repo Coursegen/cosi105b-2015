ba682a0d12604d50dab228124a2c2f3d031f7f07
Pito Salas <pitosalas@gmail.com>
2014-11-16 18:09:04 -0500

    first commit

---
+++ cg_config.rb
@@ -0,0 +1,19 @@
+require 'coursegen'
+
+# Copyright string
+COPYRIGHT_STRING = "Copyright (2014-2015) R. Pito Salas, pitosalas@gmail.com"
+
+# bucket for AWS Deployment of the course
+AWS_BUCKET = "cosi105b.2015.courses.salas.com"
+
+# Course short name
+COURSE_SHORT_NAME = "Cosi 105b"
+COURSE_LONG_NAME = "Software Engineering and architecture at scale"
+
+# Sections in the right hand margin of the page
+SECTION_CONFIG = [
+  SectionDef.new("Background", "background", type: :section),
+  SectionDef.new("NanoTwitter", "nt", type: :section),
+  SectionDef.new("Hidden", "hidden", hidden: true, type: :section)
+]
+
