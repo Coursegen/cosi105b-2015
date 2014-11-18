b23335d4080ee4cba9e69baff5d93ab0002c8757
Pito Salas <pitosalas@gmail.com>
2014-11-18 17:01:20 -0500

    added more content

--- cg_config.rb
+++ cg_config.rb
@@ -10,10 +10,20 @@ AWS_BUCKET = "cosi105-2015"
 COURSE_SHORT_NAME = "Cosi 105b"
 COURSE_LONG_NAME = "Software Engineering and architecture at scale"
 
+# Schedule information. Note that Monday is day 0
+LECTURES_SCHEDULE_CONFIG = ScheduleDef.new(
+    first_day: "jan-12-2015", 
+    weekdays: [:monday, :wednesday],
+    number: 26,
+    skips: [])
+
+
 # Sections in the right hand margin of the page
 SECTION_CONFIG = [
+	SectionDef.new("Lectures", "lectures", type: :lecture, schedule: LECTURES_SCHEDULE_CONFIG),
+	SectionDef.new("Introduction", "intro", hidden: false, type: :section),
   SectionDef.new("Background", "background", type: :section),
   SectionDef.new("NanoTwitter", "nt", type: :section),
+	SectionDef.new("Topics", "topics", hidden: true, type: :section),
   SectionDef.new("Hidden", "hidden", hidden: true, type: :section)
 ]
-
