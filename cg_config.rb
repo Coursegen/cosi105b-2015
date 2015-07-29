303cd27509fc6c5c40cf30a2a8f146a9e8c4c83a
Pito Salas <pitosalas@gmail.com>
2015-07-29 13:13:27 -0400

    minor change to accomodate nanoc4

--- cg_config.rb
+++ cg_config.rb
@@ -12,16 +12,15 @@ COURSE_LONG_NAME = "Software Engineering and Architecture at Scale"
 
 # Schedule information. Note that Monday is day 0
 LECTURES_SCHEDULE_CONFIG = ScheduleDef.new(
-    first_day: "jan-12-2015", 
-    weekdays: [:monday, :wednesday],
+    first_day: "aug-27-2015",
+    weekdays: [:tuesday, :thursday],
     number: 26,
-    skips: ["jan-19-2015", "jan-26-2015", "jan-28-2015", 
-            "feb-16-2015", "feb-18-2015", "apr-6-2015", "apr-8-2015"])
+    skips: [])
 
 
 # Sections in the right hand margin of the page
 SECTION_CONFIG = [
-  SectionDef.new("Intro", "intro", type: :section, bullet: :star),	
+  SectionDef.new("Intro", "intro", type: :section, bullet: :star),
 	SectionDef.new("Lectures", "lectures", type: :lecture, schedule: LECTURES_SCHEDULE_CONFIG, bullet: :dash),
   SectionDef.new("Background", "background", type: :section),
   SectionDef.new("NanoTwitter", "nt", type: :section),
